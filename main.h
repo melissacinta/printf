8d5cb75a51e69d84d9419bbf7652365c5cc4c147
Love <mzeelovegeneral@gmail.com>
2023-08-19 22:21:10 +0100

    updated header and _printf file

--- main.h
+++ main.h
@@ -4,8 +4,19 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-int _putchar(char c);
+/**
+ * struct formats - a data type, holds the variety of specifiers
+ * @spec: a char pointer to hold individual specifier type
+ * @f_ptr:a funtion pointer that  executes corresponding function to the spec.
+ */
+struct formats
+{
+	char *spec;
+	int (*f_ptr)();
+}specifier;
 
+
+int _putchar(char c);
 int _printf(const char *format, ...);
 
 
