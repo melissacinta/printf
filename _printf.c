3b70cfff7ce913bcb4ffb898a30cdde06f6f9a61
Love <mzeelovegeneral@gmail.com>
2023-08-21 21:31:14 +0100

    encodes passed string in ROT-13 format

--- _printf.c
+++ _printf.c
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 
 	specifier arr[] = {{"%d", print_dec}, {"%c", print_char},
 	{"%s", print_string}, {"%i", print_int}, {"%%", print_modulus},
-	{"%b", print_binary}};
+	{"%b", print_binary}, {"%R", print_rot_13}};
 
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
@@ -25,7 +25,7 @@ Loop:
 		 * intialize this to (arr.length - 1). i am using 0 because
 		 * currently specifiers array only contains one item which is at index 0
 		 */
-		j = 5;
+		j = 6;
 		while (j >= 0)
 		{
 			/**
