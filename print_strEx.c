9011b586b412d7a9b1b34e3a6e3f3a1840988f98
Ngozi <melissacinta@gmail.com>
2023-08-22 21:58:09 +0000

    trying a fix for printing special characters

--- print_strEx.c
+++ print_strEx.c
@@ -12,7 +12,7 @@ int print_strEx(va_list data)
 
 	while (*str != '\0')
 	{
-		if (*str < 32 || *str >= 127)
+		if (*str < 32 || *str == '\n' || *str >= 127)
 		{
 			count += _putchar('\\');
 			count += _putchar('x');
